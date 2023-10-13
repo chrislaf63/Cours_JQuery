0602993bbdfd1c8b804665ccbf0e1a80c3a0e32b
chrislaf63 <chrislaf63@gmail.com>
2023-10-13 10:52:13 +0200

    CSS

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $(function(){
   
-    $("#start").click(function(){
-        $("div").empty();
+    $("#but1").click(function(){
+
+        ( $("#but2").width(150))
+          
+    
+       
     });
+
+    // $("#but2").click(function(){
+    //     $("#p1").removeClass("red");
+    // });
     
 
 });
